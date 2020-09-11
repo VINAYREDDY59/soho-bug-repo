45cb87c41b9d05598cefbc263d4a5f5d9702a89b
Ahn <anhpnnd@gmail.com>
2020-09-11 12:19:37 +0200

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { NgxsModule } from '@ngxs/store';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     NgxsModule.forRoot([]),
   ],
   providers: [],
--- src/app/data-grid/health-data.service.ts
+++ src/app/data-grid/health-data.service.ts
@@ -1,35 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class HealthDataService {
 
+  constructor(private httpClient: HttpClient) {
+  }
+
   getData$(): Observable<any[]> {
-    return of([
-      {
-        id: 1,
-        name: 'health monitor 1',
-        description: 'description',
-        lastUpdatedBy: 'Jeroen',
-        lastUpdatedOn: '9/2/2020',
-        status: 'inactive',
-      },
-      {
-        id: 2,
-        name: 'health monitor 2',
-        lastUpdatedBy: 'Anh',
-        status: 'active',
-      },
-      {
-        id: 3,
-        name: 'health monitor 3',
-        lastUpdatedBy: 'Jeroen',
-        lastUpdatedOn: '9/2/2020',
-        status: 'inactive',
-      },
-    ]).pipe(
-      delay(1000),
-    );
+    return this.httpClient.get<any[]>('http://localhost:3004/health-monitors');
   }
 }
